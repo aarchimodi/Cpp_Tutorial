ea6c048236b758877c5915c4adda93e4dc614855
Aarchi Modi <117932380+aarchimodi@users.noreply.github.com>
2024-01-15 02:32:48 +0530

    Update vars_datatypes.cpp

--- vars_datatypes.cpp
+++ vars_datatypes.cpp
@@ -5,21 +5,21 @@ int main(){
 
   
   // these 3 datatypes are used for integers
-  int a = 5;        //int stores 4 bytes of memory
+  int a = 5;                //int stores 4 bytes of memory
   cout << a << '\n';
 
-  long a1 = 67568; //to store wider range of numbers
-  cout << a1 <<'\n';
+  long a1 = 67568;          //to store wider range of numbers 
+  cout << a1 <<'\n';        //8 bytes of memory
 
-  long long a2 = 74868946;  //to store very very long range of numbers
-  cout << a2 << '\n';
+  long long a2 = 74868946;  //to store very very long range of numbers 
+  cout << a2 << '\n';       //8 bytes of memory
 
 
 
 //  these 2 datatypes are used for characters and strings
 // note : char datatype will store only one character we cannot store string into char
 
-  char ch = 'a';    //char stores 1 byte of memory
+  char ch = 'a';            //char stores 1 byte of memory
   cout << ch <<'\n';
 
   string str = "My name is aarchi";
@@ -37,13 +37,13 @@ int main(){
   double d = 1.23;   //double stores 8 bytes of memory
   cout << d <<'\n' ;
 
-  int size = sizeof(a);  //sizeof operator 
-  cout << size <<'\n';
-
 
   // to store both positive/negative values we use signed
-  signed int a4 = -5;    //signed/unsigned stores 8 bytes of memeory
+  signed int a4 = -5;    //signed/unsigned stores 4 bytes of memeory
   cout << a4 <<'\n';
   
+
+  int size = sizeof(a4);  //sizeof operator 
+  cout << size <<'\n';
   return 0;
 }
